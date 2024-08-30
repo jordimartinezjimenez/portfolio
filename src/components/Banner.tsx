90731bf2fc5ba85320e7f94653bd915923e5111a
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-08-30 12:25:15 +0200

    Resume link fixed and some btn styles

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -10,8 +10,8 @@ export default function Banner() {
                 <p className="text-2xl lg:text-4xl">I am a Front-End Developer</p>
                 <p className="mx-auto max-w-lg mb-8 text-center lg:text-left lg:mx-0">Passionate about web design and development.<br />Highly creative and imaginative.</p>
                 <div className="flex gap-6 items-center mb-8">
-                    <a href="#contact" className="flex items-center justify-center bg-gradient-to-r from-rose-500 to-pink-500 rounded-lg gap-2 py-1.5 px-8 cursor-pointer shadow-lg shadow-rose-500/50"><FaEnvelope color="white" />Contact</a>
-                    <a href="#" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center bg-gradient-to-r from-rose-500 to-pink-500 rounded-lg gap-2 py-1.5 px-8 cursor-pointer shadow-lg shadow-rose-500/50"><FaCloudDownloadAlt color="white"  />Resume</a>
+                    <a href="#contact" className="flex items-center justify-center bg-gradient-to-r from-rose-500 to-pink-500 rounded-lg gap-2 py-1.5 px-8 cursor-pointer shadow-lg shadow-rose-500/50 hover:scale-110 transition-all duration-[400ms] ease"><FaEnvelope color="white" />Contact</a>
+                    <a href="./public/CV-Jordi_Martinez_Jimenez.pdf" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center bg-gradient-to-r from-rose-500 to-pink-500 rounded-lg gap-2 py-1.5 px-8 cursor-pointer shadow-lg shadow-rose-500/50 hover:scale-110 transition-all duration-[400ms] ease"><FaCloudDownloadAlt color="white"  />Resume</a>
                 </div>
                 <div className="flex gap-x-6 max-w-max mx-auto lg:mx-0">
                     <a href="https://www.linkedin.com/in/jordi-martinez-jimenez/" target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-all duration-[400ms] ease"><FaLinkedin color="#334155" size={35} /></a>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { FaRegFileAlt } from "react-icons/fa"
 export default function Header() {
     return (
         <header className="flex justify-between items-center py-8">
-            <a href="#home"><img src={logo} alt="logo" className="w-[124px] cursor-pointer" /></a>
-            <a href="#contact" className="flex items-center justify-center bg-gradient-to-r from-rose-500 to-pink-500 rounded-lg gap-2 py-1.5 px-8 cursor-pointer shadow-lg shadow-rose-500/50"><FaRegFileAlt /> Hire me</a>
+            <a href="#home"><img src={logo} alt="logo" className="w-[124px] cursor-pointer hover:scale-110 transition-all duration-[400ms] ease" /></a>
+            <a href="#contact" className="flex items-center justify-center bg-gradient-to-r from-rose-500 to-pink-500 rounded-lg gap-2 py-1.5 px-8 cursor-pointer shadow-lg shadow-rose-500/50  hover:scale-110 transition-all duration-[400ms] ease"><FaRegFileAlt /> Hire me</a>
         </header>
     )
 }
